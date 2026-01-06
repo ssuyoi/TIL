5b8a14f9d9740fc671fd67064794c2deb0c4112d
sua <log.ddua@gmail.com>
2026-01-07 00:58:03 +0900

    [2026-01-06] Java 클래스, 기본형과 참조형

---
+++ lectures/java/basic/ref/ex/ProductOrderMain2.java
@@ -0,0 +1,55 @@
+package basic.ref.ex;
+
+import java.util.Scanner;
+
+public class ProductOrderMain2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("입력할 주문의 개수를 입력하세요: ");
+        int orderCount = sc.nextInt();
+
+        ProductOrder[] orders = new ProductOrder[orderCount];
+
+        for(int i = 0; i < orderCount; i++) {
+            System.out.println((i+1) + "번째 주문 정보를 입력하세요.");
+
+            System.out.print("상품명: ");
+            String productName = sc.next();
+
+            System.out.print("가격: ");
+            int price = sc.nextInt();
+
+            System.out.print("수량: ");
+            int quantity = sc.nextInt();
+            sc.nextLine();
+
+            orders[i] = createOrder(productName, price, quantity);
+        }
+        printOrders(orders);
+        int totalAmount = getTotalAmount(orders);
+        System.out.println("총 결제 금액: " + totalAmount);
+    }
+
+    static ProductOrder createOrder(String productName, int price, int quantity) {
+        ProductOrder order = new ProductOrder();
+        order.productName = productName;
+        order.price = price;
+        order.quantity = quantity;
+        return order;
+    }
+
+    static void printOrders(ProductOrder[] orders) {
+        for (ProductOrder o : orders) {
+            System.out.println("상품명: " + o.productName + ", 가격: " + o.price + ", 수량: " + o.quantity);
+        }
+    }
+
+    static int getTotalAmount(ProductOrder[] orders) {
+        int amount = 0;
+        for(ProductOrder o : orders) {
+            amount += o.price * o.quantity;
+        }
+        return amount;
+    }
+}
